c8e27459d24f605078fb5abc442316a6eeb949e4
Matei Radu <opensource@mateiradu.dev>
2023-03-12 21:23:29 +0100

    Add first iteration of NamespacedStorage
    This initial version covers the main methods of the `Storage` interface
    and the `length` property. Any other feature or behavior of the
    `Storage` interface is not implemented.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -14,17 +14,83 @@
  * limitations under the License.
  */
 
-import test from "ava";
-import { greet } from "./index.js";
+import anyTest, { TestFn } from "ava";
+import { NamespacedStorage } from "./index.js";
+import StorageShim from "node-storage-shim";
 
-test("greet - uses default value", (t) => {
-  const greetMsg = greet();
+type TestContext = {
+  storage: Storage;
+};
 
-  t.is(greetMsg, "Hello, World!");
+const test = anyTest as TestFn<TestContext>;
+
+test.before((t) => {
+  t.context.storage = new StorageShim();
 });
 
-test("greet - uses provided name", (t) => {
-  const greetMsg = greet("Matt");
+test.beforeEach((t) => {
+  t.context.storage.clear();
+});
 
-  t.is(greetMsg, "Hello, Matt!");
+test("NamespacedStorage - constructor", (t) => {
+  t.notThrows(() => new NamespacedStorage(t.context.storage, "foo"));
+});
+
+test("NamespacedStorage - setItem", (t) => {
+  const nsStorage = new NamespacedStorage(t.context.storage, "foo");
+  nsStorage.setItem("user-id", "1234");
+
+  t.is(t.context.storage.getItem("foo:user-id"), "1234");
+});
+
+test("NamespacedStorage - getItem", (t) => {
+  const nsStorage = new NamespacedStorage(t.context.storage, "foo");
+  t.context.storage.setItem("foo:user-id", "1234");
+  t.context.storage.setItem("user-id", "5678");
+
+  t.is(nsStorage.getItem("user-id"), "1234");
+});
+
+test("NamespacedStorage - removeItem", (t) => {
+  const nsStorage = new NamespacedStorage(t.context.storage, "foo");
+  t.context.storage.setItem("foo:user-id", "1234");
+  t.context.storage.setItem("user-id", "5678");
+
+  nsStorage.removeItem("user-id");
+
+  t.is(t.context.storage.getItem("foo:user-id"), null);
+  t.is(t.context.storage.getItem("user-id"), "5678");
+});
+
+test("NamespacedStorage - clear", (t) => {
+  const nsStorage = new NamespacedStorage(t.context.storage, "foo");
+  t.context.storage.setItem("foo:user-id", "1234");
+  t.context.storage.setItem("foo:preferred-theme", "dark");
+  t.context.storage.setItem("user-id", "5678");
+
+  nsStorage.clear();
+
+  t.is(t.context.storage.getItem("foo:user-id"), null);
+  t.is(t.context.storage.getItem("foo:preferred-theme"), null);
+  t.is(nsStorage.length, 0);
+  t.is(t.context.storage.getItem("user-id"), "5678");
+});
+
+test("NamespacedStorage - length", (t) => {
+  const nsStorage = new NamespacedStorage(t.context.storage, "foo");
+  t.context.storage.setItem("foo:user-id", "1234");
+  t.context.storage.setItem("foo:preferred-theme", "dark");
+  t.context.storage.setItem("user-id", "5678");
+
+  t.is(nsStorage.length, 2);
+});
+
+test("NamespacedStorage - key", (t) => {
+  const nsStorage = new NamespacedStorage(t.context.storage, "foo");
+  t.context.storage.setItem("foo:user-id", "1234"); // idx 0, ns_idx 0
+  t.context.storage.setItem("user-id", "5678"); // idx 1, ns_idx -
+  t.context.storage.setItem("foo:preferred-theme", "dark"); // idx 2, ns_idx 1
+
+  t.is(nsStorage.key(0), "user-id");
+  t.is(nsStorage.key(1), "preferred-theme");
 });
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,88 @@
  * limitations under the License.
  */
 
-export const greet = (name?: string): string => {
-  return `Hello, ${name ?? "World"}!`;
+const PREFIX_SEPARATOR = ":";
+
+const prefixKey = (prefix: string, key: string): string => {
+  return [prefix, key].join(PREFIX_SEPARATOR);
 };
+
+const removePrefixFromKey = (prefix: string, prefixedKey: string): string => {
+  const prefixTotalLength = prefix.length + PREFIX_SEPARATOR.length;
+
+  return prefixedKey.slice(prefixTotalLength);
+};
+
+const isKeyPrefixed = (key: string, prefix: string): boolean => {
+  return key.startsWith(prefix + PREFIX_SEPARATOR);
+};
+
+export class NamespacedStorage implements Storage {
+  /**
+   * Creates a new instance of `NamespacedStorage` based on the given
+   * `storage` implementation.
+   *
+   * All methods will operate on the given `storage` object using the given
+   * `prefix` to create a namespace for keys.
+   */
+  constructor(private storage: Storage, private prefix: string) {}
+
+  /** Returns the number of key/value pairs in the namespace. */
+  public get length(): number {
+    return this.getNamespacedEntries().length;
+  }
+
+  /**
+   * Sets the given key/value pair in the namespace. If an entry with the given
+   * key already exists, its value will be updated.
+   */
+  public setItem(key: string, value: string): void {
+    return this.storage.setItem(prefixKey(this.prefix, key), value);
+  }
+
+  /**
+   * Returns the current value associated with the given key in the namespace,
+   * or null if the given key does not exist.
+   */
+  public getItem(key: string): string | null {
+    return this.storage.getItem(prefixKey(this.prefix, key));
+  }
+
+  /**
+   * Removes the key/value pair with the given key in the namespace, if a
+   * key/value pair with the given key exists.
+   */
+  public removeItem(key: string): void {
+    return this.storage.removeItem(prefixKey(this.prefix, key));
+  }
+
+  /**
+   * Removes all key/value pairs in the namespace, if there are any.
+   *
+   * Key/value pairs outside the namespace are unaffected.
+   */
+  public clear(): void {
+    this.getNamespacedEntries().forEach(([key]) =>
+      this.storage.removeItem(key)
+    );
+  }
+
+  /**
+   * Returns the name of the nth key in the namespace. If the index does not
+   * exist, null is returned.
+   *
+   * The order of keys for the regular Storage API is user-agent defined, so
+   * it is quite unreliable. Consequently, this namespaced implementation is
+   * also very unreliable. Use it at your own risk.
+   */
+  public key(index: number): string | null {
+    const key = this.getNamespacedEntries()[index][0];
+    return key ? removePrefixFromKey(this.prefix, key) : null;
+  }
+
+  private getNamespacedEntries(): [string, string | null][] {
+    return Object.entries(this.storage).filter(([key]) =>
+      isKeyPrefixed(key, this.prefix)
+    );
+  }
+}
